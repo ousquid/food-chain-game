ffede13cd251e530259d770cfd29986ac21df53d
koiking213 <koiking213@gmail.com>
2022-07-06 22:01:24 +0900

    add player

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 const UNIT_WIDTH: u32 = 40;
 const UNIT_HEIGHT: u32 = 40;
@@ -12,11 +13,30 @@ const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
-            title: "food chain game".to_string(),
+            title: "FoodChainGame".to_string(),
             width: SCREEN_WIDTH as f32,
             height: SCREEN_HEIGHT as f32,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+        .add_startup_system(setup_system)
         .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::Circle {
+        radius: 20.,
+        center: Vec2::new(0.0, 0.0),
+    };
+
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(GeometryBuilder::build_as(
+        &shape,
+        DrawMode::Outlined {
+            fill_mode: FillMode::color(Color::PURPLE),
+            outline_mode: StrokeMode::new(Color::BLACK, 0.0),
+        },
+        Transform::default(),
+    ));
 }
