2e8a64efabdb92059f9c7c9f6e23e93611773753
koiking213 <koiking213@gmail.com>
2022-10-19 21:09:33 +0900

    バランス調整

--- src/consts.rs
+++ src/consts.rs
@@ -23,14 +23,14 @@ pub const SCREEN_HEIGHT: u32 = 36;
 // Food Spawn
 pub const INITIAL_BEAR_NUM: u32 = 1;
 pub const INITIAL_FOX_NUM: u32 = 3;
-pub const INITIAL_WALNUT_NUM: u32 = 15;
+pub const INITIAL_WALNUT_NUM: u32 = 10;
 
 // probability (5% = 500)
-pub const PROBABILITY_INCREASE_WALNUT: u32 = 200;
+pub const PROBABILITY_INCREASE_WALNUT: u32 = 300;
 
 // Game Tick
 // 1Game 3.5 min = 210 sec = 2100 tick
-pub const GAME_DEFAULT_FPS: i32 = 30;
+pub const GAME_DEFAULT_FPS: i32 = 10;
 // pub const GAME_FPS: i32 = GAME_DEFAULT_FPS; // 100 / GAME_TICK as i32;
 pub const GAME_FPS: i32 = 10;
 pub const GAME_TICK: u64 = 1000 / GAME_FPS as u64; // 10; // ms
@@ -50,20 +50,20 @@ pub const MAX_HP_WALNUT: f32 = 1.0;
 
 pub const DECREASE_HP_HUMAN: f32 = 1.0 / GAME_DEFAULT_FPS as f32; // eat fox -> 10sec eat bear-> 30sec
 pub const DECREASE_HP_BEAR: f32 = 0.3 / GAME_DEFAULT_FPS as f32; // eat fox -> 30sec
-pub const DECREASE_HP_FOX: f32 = 0.1 / GAME_DEFAULT_FPS as f32;
+pub const DECREASE_HP_FOX: f32 = 0.2 / GAME_DEFAULT_FPS as f32;
 pub const DECREASE_HP_WALNUT: f32 = 0.0;
 
 pub const INITIAL_HP_HUMAN: f32 = 50.0;
 
 pub const HEALING_HP_HUMAN: f32 = 20.0;
 pub const HEALING_HP_BEAR: f32 = 30.0;
-pub const HEALING_HP_FOX: f32 = 10.0;
+pub const HEALING_HP_FOX: f32 = 5.0;
 pub const HEALING_HP_WALNUT: f32 = 1.0;
 
 // Stamina
 pub const MAX_STAMINA: i32 = 100;
 
-pub const HEALING_STAMINA_HUMAN: i32 = 10;
+pub const HEALING_STAMINA_HUMAN: i32 = 12;
 pub const HEALING_STAMINA_STRONG_BEAR: i32 = 10;
 pub const HEALING_STAMINA_WEAK_BEAR: i32 = 10;
 pub const HEALING_STAMINA_FOX: i32 = 10;
